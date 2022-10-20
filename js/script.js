f4c7e872057f6ab3497002ded83c3af6d55a5f10
Naveos <108453538+Naveos@users.noreply.github.com>
2022-10-20 08:57:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -216,7 +216,7 @@ function login() {
                 document.getElementById("reponseLogin").innerHTML = ("Redirection...");
             }, 3000);
             setTimeout(() => {
-                document.location.href="../html/utilisateurs/sam.html";
+                document.location.href="html/utilisateurs/sam.html";
             }, 3000);
         }
         else{
@@ -229,11 +229,11 @@ function login() {
 }
 jQuery(function(){
     $(function () {
-        $(window).scroll(function () { //Fonction appelée quand on descend la page
-            if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
-                $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200 ) {
+                $('#scrollUp').css('right','10px');
             } else { 
-                $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+                $('#scrollUp').removeAttr( 'style' );
             }
         });
     });
